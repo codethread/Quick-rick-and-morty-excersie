b4c1d86285791addddd7bc8ce71416e737b15e42
Francesca <francesca.dellanno@sky.uk>
2019-08-27 20:23:08 +0100

    update function names and remove duplicate functions

--- index.js
+++ index.js
@@ -8,78 +8,77 @@ const rp = require('request-promise');
  * return the results as an array of names, without duplicates
  */
 
-const RICK = 1;
+const characterDetails = {
+  id: 1,
+  name: 'Rick'
+};
 
-// break down into smaller functions
-// update remaining reduces
-// Add in slice
 // Typescript
+// Look through rambda for naming conventions
+// get ids from urls (function duplicated)
+// change From to from and information to data
 
-getCharacterInfo(RICK)
-  .then(getCharacterIdsAssociatedWithAnotherCharacter)
-  .then(getCharacterNamesFromListOfIds);
-
-async function getCharacterInfo(characterId) {
-  const characterInfo = await rp(
-    `https://rickandmortyapi.com/api/character/${characterId}`
-  ).then(characterInfoData => {
-    const { id, name, episode } = JSON.parse(characterInfoData);
-
-    // SLICE rather than replace
-    const episodeNumbers = episode.map(episodeURL =>
-      episodeURL.replace('https://rickandmortyapi.com/api/episode/', '')
+getCharacterDataFromCharacterIds(characterDetails.id)
+  .then(getEpisodeUrlsFromCharacterData)
+  .then(getIdsFromUrls)
+  .then(getEpisodeDataFromEpisodeIds)
+  .then(getCharacterUrlsFromEpisodeData)
+  .then(dedupe)
+  .then(removeCharacterUrlFor(characterDetails.id))
+  .then(getIdsFromUrls)
+  .then(getCharacterDataFromCharacterIds)
+  .then(getCharacterNamesFromCharacterData)
+  .then(characterNames => {
+    console.log(
+      `${characterDetails.name} has met 
+      ${characterNames.length} characters, including: `,
+      characterNames
     );
-
-    return {
-      id,
-      name,
-      episodeNumbers
-    };
   });
-  return characterInfo;
+
+async function getCharacterDataFromCharacterIds(characterIds) {
+  return rp({
+    uri: `https://rickandmortyapi.com/api/character/${characterIds}`,
+    json: true
+  });
 }
 
-async function getCharacterIdsAssociatedWithAnotherCharacter(characterInfo) {
-  const { id, episodeNumbers } = characterInfo;
-  const associatedCharacterIds = await rp({
-    uri: `https://rickandmortyapi.com/api/episode/${episodeNumbers}`,
+async function getEpisodeUrlsFromCharacterData({ episode }) {
+  return episode;
+}
+
+async function getIdsFromUrls(urls) {
+  return urls.map(url => url.split('/').pop());
+}
+
+async function getEpisodeDataFromEpisodeIds(episodeIds) {
+  return rp({
+    uri: `https://rickandmortyapi.com/api/episode/${episodeIds}`,
     json: true
-  }).then(multipleEpisodesData => {
-    const charactersWithDuplicates = multipleEpisodesData.reduce(
-      (characters, episode) => {
-        return [...characters, ...episode.characters];
-      },
-      []
-    );
+  });
+}
 
-    const charactersWithoutDuplicates = Array.from(
-      new Set(charactersWithDuplicates)
-    );
+async function getCharacterUrlsFromEpisodeData(episodeData) {
+  return episodeData.reduce((characters, episode) => {
+    return [...characters, ...episode.characters];
+  }, []);
+}
 
-    const charactersWithoutDuplicatesAndSelf = charactersWithoutDuplicates.filter(
+async function dedupe(characterUrls) {
+  return Array.from(new Set(characterUrls));
+}
+
+function removeCharacterUrlFor(characterId) {
+  return function(characterUrls) {
+    return characterUrls.filter(
       character =>
-        character !== `https://rickandmortyapi.com/api/character/${id}`
+        character !== `https://rickandmortyapi.com/api/character/${characterId}`
     );
-
-    const uniqueCharacterIds = charactersWithoutDuplicatesAndSelf.map(
-      characterURL =>
-        characterURL.replace('https://rickandmortyapi.com/api/character/', '')
-    );
-    return uniqueCharacterIds;
-  });
-  return associatedCharacterIds;
+  };
 }
 
-async function getCharacterNamesFromListOfIds(idList) {
-  const associatedCharacterNames = await rp({
-    uri: `https://rickandmortyapi.com/api/character/${idList}`,
-    json: true
-  }).then(multipleCharactersData => {
-    return multipleCharactersData.reduce((names, character) => {
-      names.push(character.name);
-      return names;
-    }, []);
+async function getCharacterNamesFromCharacterData(characterInformation) {
+  return characterInformation.map(character => {
+    return character.name;
   });
-  console.log(associatedCharacterNames);
-  return associatedCharacterNames;
 }
